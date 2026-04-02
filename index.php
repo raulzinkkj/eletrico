c802b3b5b5c26aa3cbb39ec415b04f380336734e
raulzinkkj <karvatraul@gmail.com>
2026-04-02 17:05:20 -0300

    ohm7

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        
+    </header>
+    <section>
+        <nav>
+            <ul>
+                <li><a href="eletrica2.0/lei_de_ohm/index.php">Triângulo da Lei de Ohm</a></li>
+                <li><a href="eletrica2.0/potencia/index.php">Triângulo da Potência</a></li>
+            </ul>
+        </nav>
+    </section>
+</body>
+</html>
